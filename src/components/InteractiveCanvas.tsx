7ba0fca22a6d5c6dd0ca99586c9307ae493c733d
whanso <w_hanson@outlook.com>
2025-07-24 11:23:31 -0400

    Update InteractiveCanvas.tsx

--- src/components/InteractiveCanvas.tsx
+++ src/components/InteractiveCanvas.tsx
@@ -244,7 +244,7 @@ export default function InteractiveCanvas({
         setLastCenter(newCenter);
       }
     },
-    [dragStopped, lastCenter, lastDist]
+    [dragStopped, lastCenter, lastDist, stagePos, stageScale]
   );
 
   const handleTouchEnd = () => {
